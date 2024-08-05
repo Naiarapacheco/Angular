cc9272765894e60eded4e7610a9df49a4ed14cf6
Naiara Pacheco <naiarapachecosantos@gmail.com>
2024-08-05 13:15:34 -0300

    feat: codigo desenvolvido do 04 componente

--- src/app/componente04/componente04.component.ts
+++ src/app/componente04/componente04.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-componente04',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './componente04.component.html',
   styleUrl: './componente04.component.css'
 })
 export class Componente04Component {
+
+  // Váriavel para exibir ou ocultar
+  exibir:boolean = false;
+
+  // Função para exibir ou ocultar
+  acao(){
+    if(this.exibir === true){
+      this.exibir = false;
+    }else{
+      this.exibir = true;
+    }
+  }
 
 }
